0fdba6a5a00e4e63cc7017a6dcb268a32d3a6b15
Rafal <glowka.rafal@gmail.com>
2022-08-11 12:59:24 +0200

    Graphical improvements:
    "sticky header" over the grid

--- presentation/src/main/java/com/glowka/rafal/topmusic/presentation/flow/dashboard/list/ListFragment.kt
+++ presentation/src/main/java/com/glowka/rafal/topmusic/presentation/flow/dashboard/list/ListFragment.kt
@@ -6,17 +6,21 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement.Bottom
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyGridItemScope
 import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
@@ -54,6 +58,8 @@ class ListFragment :
     setContent {
       val systemUiController = rememberSystemUiController()
       val useDarkIcons = MaterialTheme.colors.isLight
+      val systemBarsPadding = WindowInsets.systemBars.asPaddingValues()
+      val statusBarHeight = systemBarsPadding.calculateTopPadding()
 
       MaterialTheme {
         systemUiController.setSystemBarsColor(
@@ -61,31 +67,65 @@ class ListFragment :
           darkIcons = useDarkIcons
         )
         val items = viewModelState.value.items
-
+        val gridState = rememberLazyGridState()
         SwipeRefresh(
-          modifier = Modifier.systemBarsPadding().fillMaxSize(),
+          modifier = Modifier
+            .navigationBarsPadding()
+            .fillMaxSize(),
           state = rememberSwipeRefreshState(viewModelState.value.isRefreshing),
           onRefresh = { viewModel.onViewEvent(ViewEvents.RefreshList) },
         ) {
           if (items.isNotEmpty()) {
-            LazyVerticalGrid(
-              modifier = Modifier.padding(10.dp),
-              columns = GridCells.Fixed(count = 2)
-            ) {
-              header {
-                Text(
-                  modifier = Modifier.padding(Margin.m4),
-                  text = getString(R.string.list_title),
-                  color = Colors.dark,
-                  fontFamily = Fonts.Bold,
-                  fontSize = FontSize.title
-                ) // or any composable for your single row
-              }
-              items(
-                items.size,
-                null
+            val smallHeader = (gridState.firstVisibleItemIndex != 0)
+            Box(modifier = Modifier.fillMaxSize()) {
+              LazyVerticalGrid(
+                modifier = Modifier.padding(10.dp),
+                columns = GridCells.Fixed(count = 2),
+                state = gridState,
               ) {
-                AlbumItem(items[it])
+                header {
+                  Text(
+                    modifier = Modifier.padding(
+                      top = Margin.m4 + statusBarHeight,
+                      bottom = Margin.m4,
+                      start = Margin.m4,
+                      end = Margin.m4,
+                    ),
+                    text = getString(R.string.list_title),
+                    color = Colors.dark,
+                    fontFamily = Fonts.Bold,
+                    fontSize = FontSize.title
+                  ) // or any composable for your single row
+                }
+                items(
+                  items.size,
+                  null
+                ) {
+                  AlbumItem(items[it])
+                }
+              }
+              if (smallHeader) {
+                Box(
+                  modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Colors.white_transparent)
+                ) {
+                  Text(
+                    modifier = Modifier
+                      .padding(
+                        top = Margin.m3 + statusBarHeight,
+                        bottom = Margin.m3,
+                        start = Margin.m3,
+                        end = Margin.m3,
+                      )
+                      .fillMaxWidth(),
+                    text = getString(R.string.list_title),
+                    color = Colors.dark,
+                    fontFamily = Fonts.Bold,
+                    textAlign = TextAlign.Center,
+                    fontSize = FontSize.base,
+                  )
+                }
               }
             }
           } else {
--- presentation/src/main/java/com/glowka/rafal/topmusic/presentation/style/Colors.kt
+++ presentation/src/main/java/com/glowka/rafal/topmusic/presentation/style/Colors.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.graphics.Color
 object Colors {
   val dark = Color(0xFF111226)
   val white = Color(0xFFFFFFFF)
+  val white_transparent = Color(0xD9FFFFFF)
   val gray = Color(0xFFb5b5b5)
   val authorName = Color(0xFF8e8e93)
   val blue = Color(0xFF007aff)
